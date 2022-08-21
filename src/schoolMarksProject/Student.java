616df93f757fa4fb622c976e9f3f5d57d2a6e468
Maksym Linkevych <95864524+MLinkevych@users.noreply.github.com>
2022-08-21 16:01:53 +0300

    my project

--- src/schoolMarksProject/Student.java
+++ src/schoolMarksProject/Student.java
@@ -5,10 +5,21 @@ public class Student extends Parent{
     private String grade;
     private String nameStudent;
 
-    public Student(int parentID, String nameParent, String email) {
+    public Student(int parentID, String nameParent, String email, String grade, String nameStudent) {
         super(parentID, nameParent, email);
-
+        this.grade = grade;
+        this.nameStudent = nameStudent;
     }
 
+    public String getGrade() {
+        return grade;
+    }
 
+    public String getNameStudent() {
+        return nameStudent;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
 }
